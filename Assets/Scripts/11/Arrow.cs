c831f9c4a10ea881f2a10c7f7720b9a39d1f945a
24967 <forestlii@outlook.com>
2017-08-05 23:02:18 +0800

    finish Season 11

---
+++ Assets/Scripts/11/Arrow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+	private Transform m_Transoform;
+	void Start () {
+		m_Transoform = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RotateArrow();
+	}
+	private void RotateArrow()
+	{
+		m_Transoform.Rotate(new Vector3(360 * Time.deltaTime,0,0));
+	}
+}
