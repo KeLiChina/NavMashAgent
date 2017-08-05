d04ff50da1571ee9afcd3512636dca515b529c73
24967 <forestlii@outlook.com>
2017-08-05 22:12:37 +0800

    finish season 10

---
+++ Assets/Scripts/10/ParticleDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDemo : MonoBehaviour {
+
+	private ParticleSystem m_ParticleSystem;
+	void Start () 
+	{
+		m_ParticleSystem = gameObject.GetComponent<ParticleSystem>();
+	}
+	
+	void Update () {
+		ParticleCtrl();
+	}
+	private void ParticleCtrl()
+	{
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			m_ParticleSystem.Play();
+		}
+		if(Input.GetKeyDown(KeyCode.B))
+		{
+			m_ParticleSystem.Stop();
+		}
+	}
+}
