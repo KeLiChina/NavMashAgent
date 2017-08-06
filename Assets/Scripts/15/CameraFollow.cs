9abbf82858253ec49804c73aecf6e210b1cf4e15
24967 <forestlii@outlook.com>
2017-08-07 00:20:36 +0800

    finish season 15

---
+++ Assets/Scripts/15/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private Transform m_Transform;
+	private Transform player_Transform;
+	private Vector3 offset;
+	public float speed = 3f;
+	// private Vector3 temp = Vector3.zero;
+
+	void Start () {
+		m_Transform = gameObject.GetComponent<Transform>();
+		player_Transform = GameObject.Find("Necromancer").GetComponent<Transform>();
+		offset = new Vector3(-0.501f,5.423f,-2.868f);
+		// temp.y = offset.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CamFollow();
+	}
+	private void CamFollow()
+	{
+		// temp.x = Mathf.Lerp(m_Transform.position.x,player_Transform.position.x + offset.x,Time.deltaTime*speed);
+		// temp.z = Mathf.Lerp(m_Transform.position.z,player_Transform.position.z + offset.z,Time.deltaTime*speed);
+		
+
+		// m_Transform.position = temp;
+		
+
+		// easy type
+		m_Transform.position = Vector3.Lerp(m_Transform.position,player_Transform.position+offset,Time.deltaTime * speed);
+	}
+}
---
+++ Assets/Scripts/15/HouseManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseManager : MonoBehaviour {
+
+	private Transform wall_1_Transform;
+	private Transform wall_2_Transform;
+
+	void Start () {
+		wall_1_Transform = GameObject.Find("ENV/Wall_1").GetComponent<Transform>();
+		wall_2_Transform = GameObject.Find("ENV/Wall_2").GetComponent<Transform>();
+	}
+	
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			StartCoroutine(WallOpen(wall_1_Transform));
+		}
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			StartCoroutine(WallOpen(wall_2_Transform));
+		}
+		
+	}
+	IEnumerator WallOpen(Transform wall)
+	{
+		Vector3 temp = wall.position;
+		while(wall.position.y > -2.1f)
+		{
+			temp.y -= 0.1f;
+			wall.position = temp;
+			yield return new WaitForSeconds(0.05f);
+		}
+		// yield return new WaitForSeconds(1f);
+	}
+	
+}
